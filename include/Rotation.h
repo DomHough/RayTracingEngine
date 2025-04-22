0d015718bf54e48d0b47d79a20e792e4a5b548e7
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2025-04-22 11:08:55 +0100

    rotation declaration and definition

---
+++ include/Rotation.h
@@ -0,0 +1,17 @@
+//
+// Created by domho on 18/04/2025.
+//
+
+#ifndef ROTATION_H
+#define ROTATION_H
+
+
+
+class Rotation {
+public:
+    Rotation(float x=0, float y=0, float z=0);
+    float x;
+    float y;
+    float z;
+};
+#endif //ROTATION_H
