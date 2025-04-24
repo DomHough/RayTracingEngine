2e4092b5269a31246a5e208b0aa3376247b549c8
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2025-04-24 01:17:02 +0100

    segment ray intersect implementation

--- include/Sphere.h
+++ include/Sphere.h
@@ -6,8 +6,9 @@
 #define SPHERE_H
 #include <optional>
 
+#include "Material.h"
 #include "RGB.h"
-#include "Ray.h"
+#include "Rays/OriginRay.h"
 #include "Transformation.h"
 
 
@@ -16,9 +17,11 @@ public:
     Transformation transformation;
     float radius;
     RGB color;
+    Material material;
 
-    Sphere(Transformation transformation, float radius, RGB color);
-    std::optional<float> rayIntersect(const Ray ray) const;
+    Sphere(Transformation transformation, float radius, RGB color, Material material);
+    std::optional<float> rayIntersect(OriginRay ray) const;
+    std::optional<float> rayIntersect(SegmentRay ray) const;
 
 };
 
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -2,22 +2,22 @@
 // Created by domho on 18/04/2025.
 //
 
-#include "../include/Sphere.h"
+#include "Sphere.h"
 
 #include <cmath>
 #include <iostream>
 #include <optional>
 
-Sphere::Sphere(Transformation(transformation), float radius, RGB color)
-    : transformation(transformation), radius(radius), color(color) {}
+Sphere::Sphere(Transformation(transformation), float radius, RGB color, Material material)
+    : transformation(transformation), radius(radius), color(color), material(material) {}
 
 // Assume the ray is in object space
-std::optional<float> Sphere::rayIntersect(const Ray ray) const {
+std::optional<float> Sphere::rayIntersect(const OriginRay ray) const {
     float a = 1.0f;
     float b = 2.0f * ray.origin.dot(ray.direction);
     float c = ray.origin.squaredLength()- radius * radius;
 
-    float discriminant = b * b - 4 * a * c;
+    float discriminant = b * b - 4.0f * a * c;
 
     // No intersection
     if (discriminant < 0) {
@@ -34,4 +34,25 @@ std::optional<float> Sphere::rayIntersect(const Ray ray) const {
     }
 
     return 0.0f;
+}
+
+std::optional<float> Sphere::rayIntersect(const SegmentRay ray) const {
+    float a = 1.0f;
+    float b = 2.0f * ray.origin.dot(ray.direction);
+    float c = ray.origin.squaredLength()- radius * radius;
+
+    float discriminant = b * b - 4.0f * a * c;
+
+    // No intersection
+    if (discriminant < 0) {
+        return std::nullopt;
+    }
+    else {
+        float sqrt_discriminant = std::sqrt(discriminant);
+        float t1 = (-b - sqrt_discriminant) / (2 * a);
+        float t2 = (-b + sqrt_discriminant) / (2 * a);
+        if (t1 >= 1e-2f && t1 <= ray.length()-1e-2f) return t1;
+        if (t2 >= 1e-2f && t2 <= ray.length()-1e-2f) return t2;
+        return std::nullopt; // Both behind the ray
+    }
 }
