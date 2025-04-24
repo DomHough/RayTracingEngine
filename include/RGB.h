5f7cf00e9292a24fd11f57cac573bfcdced92967
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2025-04-24 01:17:37 +0100

    added operators

--- include/RGB.h
+++ include/RGB.h
@@ -4,15 +4,25 @@
 
 #ifndef RGB_H
 #define RGB_H
+#include <ostream>
 
-
+class NormalizedRGB;
 
 class RGB {
 public:
-    RGB(unsigned char r = 0, unsigned char g = 0, unsigned char b = 0);
+    RGB(int r = 0, int g = 0, int b = 0);
 
     unsigned char r;
     unsigned char g;
     unsigned char b;
+
+    RGB operator*(float scalar) const;
+    friend RGB operator*(float scalar, const RGB &color);
+    RGB operator*(const NormalizedRGB &other) const;
+    RGB operator+(const RGB& other) const;
+    RGB operator+=(const RGB& other);
+
+    friend std::ostream& operator<<(std::ostream& os, const RGB& color);
+
 };
 #endif //RGB_H
--- src/RGB.cpp
+++ src/RGB.cpp
@@ -1,6 +1,40 @@
 //
 // Created by domho on 18/04/2025.
 //
-#include "../include/RGB.h"
+#include "RGB.h"
+#include "NormalizedRGB.h"
 
-RGB::RGB(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {}
+#include <algorithm>
+#include <ostream>
+
+RGB::RGB(int r, int g, int b)
+    : r(static_cast<unsigned char>(std::clamp(r, 0, 255))),
+      g(static_cast<unsigned char>(std::clamp(g, 0, 255))),
+      b(static_cast<unsigned char>(std::clamp(b, 0, 255))) {}
+
+RGB RGB::operator*(float scalar) const {
+    return RGB(r*scalar, g*scalar, b*scalar);
+}
+
+RGB operator*(float scalar, const RGB &color) {
+    return color * scalar;
+}
+RGB RGB::operator*(const NormalizedRGB &other) const {
+    return RGB(static_cast<int>(r * other.r), static_cast<int>(g * other.g), static_cast<int>(b * other.b));
+}
+
+RGB RGB::operator+(const RGB &other) const {
+    return RGB(r+other.r, g+other.g, b+other.b);
+}
+
+RGB RGB::operator+=(const RGB &other) {
+    r = std::clamp(r + other.r, 0, 255);
+    g = std::clamp(g + other.g, 0, 255);
+    b = std::clamp(b + other.b, 0, 255);
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const RGB& color) {
+    os << "RGB(" << (int)color.r << ", " << (int)color.g << ", " << (int)color.b << ")";
+    return os;
+}
